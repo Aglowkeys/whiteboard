a2f5e9cca1e32e624957907aaae75386ccfeae76
Aglowkeys <emilianoalfonso@outlook.com>
2021-05-16 21:34:06 -0300

    Random color & input color now work

--- scripts.js
+++ scripts.js
@@ -13,25 +13,26 @@ const coords = {
 	y: null,
 };
 
-let radius = 5;
-let hue = 15;
+let radius = 5; // para el input range
+let color = '#000'; // para el selector de color
+let hue = 15; // para el input rainbow
+let rainbowColor = `hsl(${hue}, 80%, 70%)`;
 
 const drawCircle = () => {
-	ctx.fillStyle = getRandomColor();
+	ctx.fillStyle = color;
 	ctx.beginPath();
 	ctx.arc(coords.x, coords.y, radius, 0, Math.PI * 2);
 	ctx.fill();
 };
 
 const getRandomColor = () => {
-	const h = Math.floor(Math.random() * 360);
-	const s = Math.floor(Math.random() * 100);
-	const l = Math.floor(Math.random() * 100);
-
-	return `hsl(${h}, ${s}%, ${l}%)`;
+	// No puede ser en hsl porque uso este valor para
+	// reflejar el cambio también en el input type="color"
+	// y solo acepta values que sean hexadecimales.
+	color = '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
 };
 
-window.addEventListener('click', (ev) => {
+window.addEventListener('mousedown', (ev) => {
 	coords.x = ev.x;
 	coords.y = ev.y;
 	drawCircle();
@@ -41,10 +42,30 @@ window.addEventListener('click', (ev) => {
 	=== DOM TOOLS ===
 */
 
-const inputRange = document.querySelector('.input-range');
+const [btnBrush, btnEraser, btnRainbow, btnRandom, btnClear] = document.querySelectorAll('button');
+const [inputColor, inputRange] = document.querySelectorAll('input');
 const brushSize = document.querySelector('.brush-size');
 
+// Pincel
+
+// Borrador
+btnEraser.addEventListener('click', () => {});
+
+// Selector de color
+inputColor.addEventListener('change', (ev) => {
+	color = ev.target.value;
+});
+
+// Arcoiris
+
+// Color random
+btnRandom.addEventListener('click', () => {
+	getRandomColor();
+	inputColor.value = color;
+});
+
+// Tamaño pincel
 inputRange.addEventListener('input', (ev) => {
-	brushSize.innerText = ev.target.value + 'px';
+	brushSize.innerText = ev.target.value * 2 + 'px';
 	radius = ev.target.value;
 });
