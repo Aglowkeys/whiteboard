f3742e72ede09088edd381a60dcbf2d9745e7fec
Aglowkeys <emilianoalfonso@outlook.com>
2021-05-22 21:34:38 -0300

    Added rainbow functionality and drawing on mousedown

--- scripts.js
+++ scripts.js
@@ -3,28 +3,67 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-window.addEventListener('resize', () => {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-});
-
 const coords = {
 	x: null,
 	y: null,
 };
 
+const reposition = (ev) => {
+	coords.x = ev.x - canvas.offsetLeft;
+	coords.y = ev.y - canvas.offsetTop;
+};
+
+const draw = (ev) => {
+	ctx.beginPath();
+	ctx.lineWidth = radius;
+	ctx.lineCap = 'round';
+	ctx.strokeStyle = isRainbow ? rainbowColor : color;
+	ctx.moveTo(coords.x, coords.y);
+	reposition(ev);
+	ctx.lineTo(coords.x, coords.y);
+	ctx.stroke();
+
+	if (isRainbow) {
+		hue += 1;
+		rainbowColor = `hsl(${hue}, 80%, 70%)`;
+	}
+};
+
+const beginDrawing = (ev) => {
+	window.addEventListener('mousemove', draw);
+	reposition(ev);
+};
+
+const stopDrawing = () => {
+	window.removeEventListener('mousemove', draw);
+};
+
+window.addEventListener('mousedown', beginDrawing);
+window.addEventListener('mouseup', stopDrawing);
+
+window.addEventListener('resize', () => {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+});
+
 let radius = 5; // para el input range
-let prevColor = '#000'; // para no perder el color previamente seleccionado al elegir el borrador
 let color = '#000'; // para el selector de color
 let hue = 15; // para el input rainbow
+let isRainbow = false;
 let rainbowColor = `hsl(${hue}, 80%, 70%)`;
 
-const drawCircle = () => {
-	ctx.fillStyle = color;
-	ctx.beginPath();
-	ctx.arc(coords.x, coords.y, radius, 0, Math.PI * 2);
-	ctx.fill();
-};
+// const drawCircle = () => {
+// 	ctx.fillStyle = color;
+// 	ctx.beginPath();
+// 	ctx.arc(coords.x, coords.y, radius, 0, Math.PI * 2);
+// 	ctx.fill();
+// };
+
+// window.addEventListener('mousedown', (ev) => {
+// 	coords.x = ev.x;
+// 	coords.y = ev.y;
+// 	drawCircle();
+// });
 
 const getRandomColor = () => {
 	// No puede ser en hsl porque uso este valor para
@@ -32,12 +71,6 @@ const getRandomColor = () => {
 	// y solo acepta values que sean hexadecimales.
 	color = '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
 };
-
-window.addEventListener('mousedown', (ev) => {
-	coords.x = ev.x;
-	coords.y = ev.y;
-	drawCircle();
-});
 
 /* 
 	=== DOM TOOLS ===
@@ -48,13 +81,14 @@ const [inputColor, inputRange] = document.querySelectorAll('input');
 const brushSize = document.querySelector('.brush-size');
 
 // Pincel
-btnBrush.addEventListener('click', () => {
-	color = prevColor;
+btnBrush.addEventListener('mousedown', (ev) => {
+	isRainbow = false;
+	color = inputColor.value || 'black';
 });
 
 // Borrador
 btnEraser.addEventListener('click', () => {
-	prevColor = color;
+	isRainbow = false;
 	color = 'white';
 });
 
@@ -64,6 +98,9 @@ inputColor.addEventListener('change', (ev) => {
 });
 
 // Arcoiris
+btnRainbow.addEventListener('click', () => {
+	isRainbow = true;
+});
 
 // Color random
 btnRandom.addEventListener('click', () => {
