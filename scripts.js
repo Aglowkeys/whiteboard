6b769c41a469465172137fbb57012715108667c1
Aglowkeys <emilianoalfonso@outlook.com>
2021-12-26 13:51:58 -0300

    Add keyboard navigation

--- scripts.js
+++ scripts.js
@@ -4,63 +4,63 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 window.addEventListener('resize', () => {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
 
 const coords = {
-	x: null,
-	y: null,
+    x: null,
+    y: null,
 };
 
 const reposition = (ev) => {
-	coords.x = ev.x - canvas.offsetLeft;
-	coords.y = ev.y - canvas.offsetTop;
+    coords.x = ev.x - canvas.offsetLeft;
+    coords.y = ev.y - canvas.offsetTop;
 };
 
 const draw = (ev) => {
-	if (isErasing) color = canvasColor;
+    if (isErasing) color = canvasColor;
 
-	ctx.beginPath();
-	ctx.lineWidth = radius;
-	ctx.lineCap = 'round';
-	ctx.strokeStyle = isRainbow ? rainbowColor : color;
-	ctx.moveTo(coords.x, coords.y);
-	reposition(ev);
-	ctx.lineTo(coords.x, coords.y);
-	ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = radius;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = isRainbow ? rainbowColor : color;
+    ctx.moveTo(coords.x, coords.y);
+    reposition(ev);
+    ctx.lineTo(coords.x, coords.y);
+    ctx.stroke();
 
-	if (isRainbow) {
-		hue += 1;
-		rainbowColor = `hsl(${hue}, 80%, 70%)`;
-	}
+    if (isRainbow) {
+        hue += 1;
+        rainbowColor = `hsl(${hue}, 80%, 70%)`;
+    }
 };
 
 const fillCanvas = (color) => {
-	ctx.fillStyle = color;
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
 
 const beginDrawing = (ev) => {
-	canvas.addEventListener('mousemove', draw);
-	reposition(ev);
+    canvas.addEventListener('mousemove', draw);
+    reposition(ev);
 };
 
 const stopDrawing = () => {
-	canvas.removeEventListener('mousemove', draw);
+    canvas.removeEventListener('mousemove', draw);
 };
 
 canvas.addEventListener('mousedown', (ev) => {
-	if (current === btnBucket) {
-		fillCanvas(color);
-		canvasColor = color;
-	} else {
-		beginDrawing(ev);
-	}
+    if (current === btnBucket) {
+        fillCanvas(color);
+        canvasColor = color;
+    } else {
+        beginDrawing(ev);
+    }
 });
 
 canvas.addEventListener('click', (ev) => {
-	draw(ev);
+    draw(ev);
 });
 
 canvas.addEventListener('mouseup', stopDrawing);
@@ -75,122 +75,97 @@ let rainbowColor = `hsl(${hue}, 80%, 70%)`;
 
 // Puntero
 window.addEventListener('mousemove', (ev) => {
-	coords.x = ev.x;
-	coords.y = ev.y;
-	pointer.style.top = ev.y + 'px';
-	pointer.style.left = ev.x + 'px';
+    coords.x = ev.x;
+    coords.y = ev.y;
+    pointer.style.top = ev.y + 'px';
+    pointer.style.left = ev.x + 'px';
 });
 
 const getRandomColor = () => {
-	// No puede ser en hsl porque uso este valor para
-	// reflejar el cambio también en el input type="color"
-	// y solo acepta values que sean hexadecimales.
-	color = '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
+    // No puede ser en hsl porque uso este valor para
+    // reflejar el cambio también en el input type="color"
+    // y solo acepta values que sean hexadecimales.
+    color = '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
 };
 
 const updateCurrent = (elem) => {
-	current.classList.remove('active');
-	current = elem;
-	current.classList.add('active');
+    current.classList.remove('active');
+    current = elem;
+    current.classList.add('active');
 };
 
 //
 // ========== DOM TOOLS ==========
 //
 const root = document.querySelector(':root');
-const [btnBrush, btnEraser, btnRainbow, btnBucket, btnRandom, /*btnSquare, btnCircle,*/ btnClear] =
-	document.querySelectorAll('button');
-const [inputColor, inputRange /*inputUpload*/] = document.querySelectorAll('input');
+const [btnBrush, btnEraser, btnRainbow, btnBucket, btnRandom, btnClear] =
+    document.querySelectorAll('button');
+const [inputColor, inputRange] = document.querySelectorAll('input');
 const brushSize = document.querySelector('.brush-size');
 
 let current = btnBrush; // nuestra herramienta activa
 btnBrush.classList.add('active');
 
 // Pincel
-btnBrush.addEventListener('click', () => {
-	updateCurrent(btnBrush);
-	isRainbow = false;
-	isErasing = false;
-	color = inputColor.value || 'black';
-});
+const selectBrushTool = () => {
+    updateCurrent(btnBrush);
+    isRainbow = false;
+    isErasing = false;
+    color = inputColor.value || 'black';
+};
+btnBrush.addEventListener('click', selectBrushTool);
 
 // Borrador
-btnEraser.addEventListener('click', () => {
-	updateCurrent(btnEraser);
-	isRainbow = false;
-	isErasing = true;
-});
+const selectEraserTool = () => {
+    updateCurrent(btnEraser);
+    isRainbow = false;
+    isErasing = true;
+};
+btnEraser.addEventListener('click', selectEraserTool);
+
+// Arcoiris
+const selectRainbowTool = () => {
+    updateCurrent(btnRainbow);
+    isRainbow = true;
+    isErasing = false;
+};
+btnRainbow.addEventListener('click', selectRainbowTool);
+
+// Balde
+const selectBucketTool = () => {
+    isRainbow = false;
+    isErasing = false;
+    color = inputColor.value || 'black';
+    updateCurrent(btnBucket);
+};
+btnBucket.addEventListener('click', selectBucketTool);
+
+// Color random
+const selectRandomColorTool = () => {
+    getRandomColor();
+    inputColor.value = color;
+    root.style.setProperty('--current-color', color);
+};
+btnRandom.addEventListener('click', selectRandomColorTool);
 
 // Selector de color
 inputColor.addEventListener('change', (ev) => {
-	color = ev.target.value;
-	root.style.setProperty('--current-color', ev.target.value);
-});
-
-// Arcoiris
-btnRainbow.addEventListener('click', () => {
-	updateCurrent(btnRainbow);
-	isRainbow = true;
-	isErasing = false;
-});
-
-// Color random
-btnRandom.addEventListener('click', () => {
-	getRandomColor();
-	inputColor.value = color;
-	root.style.setProperty('--current-color', color);
-});
-
-// Balde
-btnBucket.addEventListener('click', () => {
-	isRainbow = false;
-	isErasing = false;
-	color = inputColor.value || 'black';
-	updateCurrent(btnBucket);
+    color = ev.target.value;
+    root.style.setProperty('--current-color', ev.target.value);
 });
 
 // Tamaño pincel
 inputRange.addEventListener('input', (ev) => {
-	radius = ev.target.value;
-	let size = radius + 'px';
-	brushSize.innerText = size;
-	pointer.style.width = size;
-	pointer.style.height = size;
+    radius = ev.target.value;
+    let size = radius + 'px';
+    brushSize.innerText = size;
+    pointer.style.width = size;
+    pointer.style.height = size;
 });
-
-//
-// ========== SUBIR IMAGEN ==========
-//
-/* 
-const isImgValid = (filename) => {
-	let extension = filename.split('.').pop().toLowerCase();
-	let validImg = ['gif', 'png', 'jpg', 'jpeg'];
-	return validImg.includes(extension);
-};
-
-inputUpload.addEventListener('change', (ev) => {
-	let file = ev.target.files[0];
-	if (isImgValid(file.name)) {
-		let fr = new FileReader();
-		fr.onload = (ev) => {
-			let img = new Image();
-			img.onload = () => {
-				// canvas.width = img.width;
-				// canvas.height = img.height;
-				ctx.drawImage(img, 100, 0);
-			};
-			img.src = ev.target.result;
-		};
-		fr.readAsDataURL(file);
-	} else {
-		alert('Archivo inválido. Formatos admitidos: png, gif, jpg');
-	}
-	inputUpload.value = '';
-});
- */
 
 // Limpiar pizarra
-btnClear.addEventListener('click', () => fillCanvas('white'));
+const selectClearBoard = () => fillCanvas('white');
+btnClear.addEventListener('click', selectClearBoard);
 
 //
 // ========== MODAL ==========
@@ -200,28 +175,29 @@ const btnClose = document.getElementById('btn-close');
 const overlay = document.getElementById('modal-overlay');
 const modal = document.getElementById('modal');
 
-btnInfo.addEventListener('click', () => {
-	overlay.classList.add('visible');
-	modal.classList.add('visible');
-});
+const openModal = () => {
+    overlay.classList.add('visible');
+    modal.classList.add('visible');
+};
+btnInfo.addEventListener('click', openModal);
 
 btnClose.addEventListener('click', () => {
-	overlay.classList.remove('visible');
-	modal.classList.remove('visible');
+    overlay.classList.remove('visible');
+    modal.classList.remove('visible');
 });
 
 overlay.addEventListener('click', (ev) => {
-	if (ev.target === overlay) {
-		overlay.classList.remove('visible');
-		modal.classList.remove('visible');
-	}
+    if (ev.target === overlay) {
+        overlay.classList.remove('visible');
+        modal.classList.remove('visible');
+    }
 });
 
 window.addEventListener('keydown', (ev) => {
-	if (ev.key === 'Escape') {
-		overlay.classList.remove('visible');
-		modal.classList.remove('visible');
-	}
+    if (ev.key === 'Escape') {
+        overlay.classList.remove('visible');
+        modal.classList.remove('visible');
+    }
 });
 
 //
@@ -230,5 +206,31 @@ window.addEventListener('keydown', (ev) => {
 const btnDownload = document.getElementById('btn-download');
 
 btnDownload.addEventListener('click', () => {
-	btnDownload.href = canvas.toDataURL('image/png');
+    btnDownload.href = canvas.toDataURL('image/png');
+});
+
+//
+// ========== SELECCIÓN CON TECLADO ==========
+//
+const selectColorTool = () => inputColor.click();
+const selectSizeTool = () => inputRange.focus();
+const selectDownloadTool = () => btnDownload.click();
+
+const keyMaps = {
+    a: selectRainbowTool,
+    b: selectEraserTool,
+    c: selectRandomColorTool,
+    d: selectDownloadTool,
+    i: openModal,
+    n: selectClearBoard,
+    p: selectBrushTool,
+    r: selectBucketTool,
+    s: selectColorTool,
+    t: selectSizeTool,
+};
+window.addEventListener('keydown', (ev) => {
+    const pressedKey = ev.key.toLowerCase();
+    if (keyMaps.hasOwnProperty(pressedKey)) {
+        keyMaps[pressedKey]();
+    }
 });
