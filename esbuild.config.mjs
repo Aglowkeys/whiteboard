a60385bb44a2f15dd457ff4721aebd458a003d8a
Aglowkeys <emilianoalfonso@outlook.com>
2024-09-11 17:21:24 -0300

    Update logging in esbuild config file

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,24 +1,17 @@
 import * as esbuild from 'esbuild';
-const TIMER_LABEL = '✨ Build successful';
 
 try {
-  console.time(TIMER_LABEL);
   const context = await esbuild.context({
     entryPoints: ['./scripts/index.ts'],
     outdir: './build',
     bundle: true,
     minify: true,
+    logLevel: 'info',
   });
-  console.timeEnd(TIMER_LABEL);
   const isWatching = process.argv.includes('--watch');
   context.watch();
 
-  if (isWatching) {
-    console.log('Watching for changes...');
-  } else {
-    context.dispose();
-  }
+  if (!isWatching) context.dispose();
 } catch (e) {
-  console.timeEnd(TIMER_LABEL);
   process.exit(1);
 }
