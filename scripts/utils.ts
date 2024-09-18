ac2c38a9cb20f8eb8f619b0487dc7e42333f91bd
Aglowkeys <emilianoalfonso@outlook.com>
2024-09-18 19:47:08 -0300

    Add util fn to set pointer bg color

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,11 +1,10 @@
-export const $ = <T extends Element = HTMLElement>(selector: string): T =>
-  document.querySelector(selector)!;
-export const $$ = <T extends Element = HTMLElement>(
-  selector: string,
-): NodeListOf<T> => document.querySelectorAll(selector);
+export const $ = <T extends Element = HTMLElement>(selector: string): T => document.querySelector(selector)!;
+export const $$ = <T extends Element = HTMLElement>(selector: string): NodeListOf<T> =>
+  document.querySelectorAll(selector);
 
-export const isTouchEvent = (ev: Event): ev is TouchEvent =>
-  ev.type.toLowerCase().startsWith('touch');
+const root = $(':root');
+
+export const isTouchEvent = (ev: Event): ev is TouchEvent => ev.type.toLowerCase().startsWith('touch');
 
 export const getRandomColor = () => {
   // No puede ser en hsl porque uso este valor para
@@ -14,4 +13,8 @@ export const getRandomColor = () => {
   const randomInt = Math.floor(Math.random() * 0xffffff);
   const hexString = randomInt.toString(16).padStart(6, '0');
   return `#${hexString}`;
+};
+
+export const setPointerBgColor = (color: string) => {
+  root.style.setProperty('--pointer-bg-color', color);
 };
