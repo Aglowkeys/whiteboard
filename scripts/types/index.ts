f433ab4073bce71aac4581871187440d9ad9e331
Aglowkeys <emilianoalfonso@outlook.com>
2024-09-19 18:03:19 -0300

    Add force param to determine brush size

--- scripts/types/index.ts
+++ scripts/types/index.ts
@@ -4,6 +4,12 @@ export type Coordinate = {
   x: number;
   y: number;
   hue?: number;
+
+  /**
+   * Present only in touch events.
+   * Indicates the pressure the user is using when drawing.
+   */
+  force?: number;
 };
 export type ContextObject = {
   main: CanvasRenderingContext2D;
